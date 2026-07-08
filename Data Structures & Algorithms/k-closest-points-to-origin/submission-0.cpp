75f5caf7ff20ee691ed92f1fd44c10bf42a642c6
Eric Song <33282538+EricSongXinLe@users.noreply.github.com>
2026-07-08 21:09:48 +0800

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        if(points.size() <= k){return points;} //they are all close.
+        vector<vector<int>> res;
+        for(auto& point: points){
+            pq.push(point);
+        }
+        int popCnt = 0;
+        while(popCnt < k){
+            res.push_back(pq.top());
+            pq.pop();
+            popCnt++;
+        }
+        return res;
+    }
+    double dist(const vector<int>& point){
+        int x = point[0];
+        int y = point[1];
+        return sqrt(x*x + y*y);
+    }
+    
+private:
+struct Compare{
+    bool operator()(vector<int>& p1, vector<int>& p2){
+        return dist(p1) > dist(p2);
+    }
+    double dist(const vector<int>& point){
+        int x = point[0];
+        int y = point[1];
+        return sqrt(x*x + y*y);
+    }
+};
+    priority_queue<vector<int>, vector<vector<int>>, Compare>pq;
+};
+
