91fac8500137db4d1b58e482f097989861f77b18
Eric Song <33282538+EricSongXinLe@users.noreply.github.com>
2026-07-29 19:53:37 +0800

    Add: house-robber-ii - submission-9

---
+++ Data Structures & Algorithms/house-robber-ii/submission-9.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int r1 = robRange(nums, 0, n - 1);
+        int r2 = robRange(nums, 1, n);
+        return max(r1,r2);
+    }
+    int robRange(vector<int>&nums, int start, int end){
+        if(start == end - 1){return nums[start];}
+        int prev2 = nums[start];
+        int prev1 = max(nums[start], nums[start+1]);
+        for(int i = start+2; i < end; i++){
+            int curr = max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
